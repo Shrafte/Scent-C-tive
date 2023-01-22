e5a005946521249b166d5c7427f6be8ad00b1f82
David Wickman <dwickma@siue.edu>
2023-01-22 16:29:01 -0600

    Changed Magic Numbers to where it detects any assignments within expressions and declarations that assigns a non-constant variable a constant numerical value. Also added detection for empty statements.

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         }
 
         // here on downwards is editable for testing purposed
-        magicNumHandler(args[0]);
+        emptyStmtHandler(args[0]);
     }
 
     private static Smell gotoHandler(String filename) {
@@ -54,10 +54,39 @@ public class Main {
         String output = "";
         String outputParse[];
         int iterator = 1;
+        boolean containsConst = false; //used for declaration statements
 
+        // handles expression statements
         do {
-            // gets each expression which involves a literal
-            argument = "string(//src:expr_stmt[src:expr[src:literal]][" + iterator + "])";
+            // gets each expression which assigns a literal number to a variable
+            argument = "string(//src:expr_stmt[src:expr[src:literal/@type='number']][" + iterator + "])";
+            ProcessBuilder builder = new ProcessBuilder("srcml", "--xpath", argument, xpathName);
+            builder.redirectError(new File("out.txt"));
+            try {
+                Process p = builder.start();
+                p.waitFor();
+                output = new String(p.getInputStream().readAllBytes());
+            } catch (IOException e) {
+                System.out.print("IOExcpetion detected");
+            } catch (InterruptedException e) {
+                System.out.print("InterruptedException detected");
+            }
+
+            //cleans the string of newline characters and splits it by space
+            output = output.replace("\n", "").replace("\r", "");
+            outputParse = output.split("\\s+");
+            if(outputParse.length > 1) {
+                System.out.println("Magic Number: " + output);
+            }
+            iterator++;
+        } while(outputParse.length > 1); // checks to see if a line was retrieved from the code. If not, end loop
+
+        iterator = 1; // reset to 1 for the next loop
+
+        // handles declaration statements that are not of constant integers
+        do {
+            // gets each declaration statement that assigns a literal number to a non-constant variable
+            argument = "string(//src:decl_stmt[src:decl/src:init/src:expr/src:literal/@type='number'][" + iterator + "])";
             ProcessBuilder builder = new ProcessBuilder("srcml", "--xpath", argument, xpathName);
             builder.redirectError(new File("out.txt"));
             try {
@@ -74,17 +103,65 @@ public class Main {
             output = output.replace("\n", "").replace("\r", "");
             outputParse = output.split("\\s+");
 
-            // if outputParse.length > 1, then xpath successfully retrieved a line from the code
-            if(outputParse.length > 1) {
+            //checks to see if 'const' type appears in the statement
+            for(String s : outputParse) {
+                if(s.equals("const")) {containsConst = true;}
+            }
 
-                //if expression statement begins with a variable that is 1 letter long, then it is a
-                //magic numbers case
-                if(outputParse[0].length() == 1 && Character.isLetter(outputParse[0].charAt(0))) {
-                    System.out.println("Magic Number: " + output);
-                }
+            if(outputParse.length > 1 && !containsConst) {
+                System.out.println("Magic Number: " + output);
             }
             iterator++;
+            containsConst = false;
         } while(outputParse.length > 1); // checks to see if a line was retrieved from the code. If not, end loop
+
+        return new Smell();
+    }
+
+    private static Smell emptyStmtHandler(String filename) {
+        System.out.println("Running XPath: Finding Empty Statements in " + filename + ":");
+        String xpathName = filename + ".xml";
+        String argument;
+        String output = "";
+        String before = "";
+        int iterator = 1;
+
+        do {
+            // gets each empty expression
+            argument = "string(//src:empty_stmt[" + iterator + "])";
+            ProcessBuilder builder = new ProcessBuilder("srcml", "--xpath", argument, xpathName);
+            builder.redirectError(new File("out.txt"));
+            try {
+                Process p = builder.start();
+                p.waitFor();
+                output = new String(p.getInputStream().readAllBytes());
+            } catch (IOException e) {
+                System.out.print("IOExcpetion detected");
+            } catch (InterruptedException e) {
+                System.out.print("InterruptedException detected");
+            }
+
+            // gets the node behind the empty expression
+            argument = "string(//src:empty_stmt[" + iterator + "]/preceding-sibling::*[position() < 2])";
+            ProcessBuilder builder2 = new ProcessBuilder("srcml", "--xpath", argument, xpathName);
+            builder.redirectError(new File("out.txt"));
+            try {
+                Process p = builder2.start();
+                p.waitFor();
+                before = new String(p.getInputStream().readAllBytes());
+            } catch (IOException e) {
+                System.out.print("IOExcpetion detected");
+            } catch (InterruptedException e) {
+                System.out.print("InterruptedException detected");
+            }
+
+            //cleans the string of newline characters and splits it by space
+            output = output.replace("\n", "").replace("\r", "");
+            before = before.replace("\n", "").replace("\r", "");
+            if(output.length() > 0) {System.out.println(before + output);}
+            iterator++;
+        } while(output.length() > 0); // checks to see if a line was retrieved from the code. If not, end loop
+
 
         return new Smell();
     }
