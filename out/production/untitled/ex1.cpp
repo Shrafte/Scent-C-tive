93f8a85cf02d98fdca5ace1772418c808fa23622
Shaun Raftery <shrafte@siue.edu>
2023-04-11 11:18:08 -0500

    rebase

---
+++ out/production/untitled/ex1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int wrong() {
+    int light;
+    int one, two, three;
+    double catDog;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void correctFunc(int x, int a) {
+    cout << "This function is named correctly" << endl;
+
+}
+
+int main() {
+    int numone;
+    int numTwo;
+    int light;
+    numone = 3;
+    numTwo = 6;
+    goto lab1;
+    goto lab2;
+    label: lab1;
+    label: lab2;
+    correctFunc(numone, numTwo);
+
+    return 0;
+}
